975b98bafdd2fe66f1701db93796e5c9f88e7a13
Vladyslav Barvinko <vlad.barvinko@gmail.com>
2024-01-14 20:55:16 +0200

    feat: create layout section "hangman"

--- app.js
+++ app.js
@@ -1,27 +1,42 @@
 document.body.innerHTML = `
   <main class="container">
-  <section class="hangman">
-    <div class="hangman__gallows">
-      <img src="./img/man/head.svg" alt="head">
-      <img src="./img/man/body.svg" alt="body">
-      <img src="./img/man/hand-one.svg" alt="hand-one">
-      <img src="./img/man/hand-two.svg" alt="hand-two">
-      <img src="./img/man/leg-one.svg" alt="leg-one">
-      <img src="./img/man/leg-two.svg" alt="leg-two">
-    </div>
-  </section>
-  <article class="question-answer">
-    <section class="answer">
+    <section class="hangman">
+      <div class="hangman__gallows">
 
+      </div>
     </section>
-    <section class="question">
-      <span class="question__hint"></span>
-      <span class="question__question"></span>
-      <span class="question__count"></span>
-    </section>
-    <section class="keyboard">
+    <article class="question-answer">
+      <section class="answer">
 
-    </section>
-  </article>
+      </section>
+      <section class="question">
+      
+      </section>
+      <section class="keyboard">
+
+      </section>
+    </article>
   </main>
 `
+function create() {
+  const hangman = document.querySelector(".hangman__gallows");
+  const question = document.querySelector(".question");
+
+  hangman.innerHTML = `
+    <img class="hangman__man hangman__head" src="./img/man/head.svg" alt="head">
+    <img class="hangman__man hangman__body" src="./img/man/body.svg" alt="body">
+    <img class="hangman__man hangman__hand-one" src="./img/man/hand-one.svg" alt="hand-one">
+    <img class="hangman__man hangman__hand-two" src="./img/man/hand-two.svg" alt="hand-two">
+    <img class="hangman__man hangman__leg-one" src="./img/man/leg-one.svg" alt="leg-one">
+    <img class="hangman__man hangman__leg-two" src="./img/man/leg-two.svg" alt="leg-two">
+  `
+
+  question.innerHTML = `
+    <span class="question__hint"></span>
+    <span class="question__question"></span>
+    <span class="question__count"></span>
+  `
+}
+
+
+create();
