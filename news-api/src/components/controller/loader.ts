3e135e3c04ced6ef8f4ad56610d9c9fd32a4ba1d
Vladyslav Barvinko <vlad.barvinko@gmail.com>
2024-02-25 17:38:22 +0200

    feat: rename files with extension js to ts, and add type "any" for get rid errors

--- news-api/src/components/controller/loader.ts
+++ news-api/src/components/controller/loader.ts
@@ -1,6 +1,9 @@
 class Loader {
-    constructor(baseLink, options) {
-        this.baseLink = baseLink;
+    private _baseLink: string;
+    private options: any;
+
+    constructor(_baseLink: string, options: any) {
+        this._baseLink = _baseLink;
         this.options = options;
     }
 
@@ -13,7 +16,7 @@ class Loader {
         this.load('GET', endpoint, callback, options);
     }
 
-    errorHandler(res) {
+    errorHandler(res: any) {
         if (!res.ok) {
             if (res.status === 401 || res.status === 404)
                 console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
@@ -23,9 +26,9 @@ class Loader {
         return res;
     }
 
-    makeUrl(options, endpoint) {
+    makeUrl(options: any, endpoint: any) {
         const urlOptions = { ...this.options, ...options };
-        let url = `${this.baseLink}${endpoint}?`;
+        let url = `${this._baseLink}${endpoint}?`;
 
         Object.keys(urlOptions).forEach((key) => {
             url += `${key}=${urlOptions[key]}&`;
@@ -34,7 +37,7 @@ class Loader {
         return url.slice(0, -1);
     }
 
-    load(method, endpoint, callback, options = {}) {
+    load(method: any, endpoint: any, callback: any, options = {}) {
         fetch(this.makeUrl(options, endpoint), { method })
             .then(this.errorHandler)
             .then((res) => res.json())
